81e49d70b3988c8b233da645018ce4a24590298f
Vishesh Handa <me@vhanda.in>
2025-09-02 19:29:20 +0200

    app: Integrate flutter_local_notifications
    A small proof of concept of sending a notification at an exact minute,
    which seems to work rather well.

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -7,10 +7,11 @@ plugins {
 
 android {
     namespace = "app.meddy"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -20,7 +21,6 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "app.meddy"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
@@ -37,6 +37,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
---
+++ app/lib/notifications.dart
@@ -0,0 +1,165 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_timezone/flutter_timezone.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest_all.dart' as tz;
+
+const appGuid = 'ce5a66ca-fd7c-4557-a9d2-038d946be413';
+const channelId = 'meddy_reminder_channel_id';
+const channelName = 'Medicine Reminder Channel';
+const channelDescription = 'Channel for Medicine reminders';
+
+final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+Future<NotificationAppLaunchDetails?> initAsyncNotifications() async {
+  // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+  const initializationSettingsAndroid = AndroidInitializationSettings(
+    'app_icon_64',
+  );
+  final initializationSettingsDarwin = DarwinInitializationSettings();
+  final initializationSettingsLinux = LinuxInitializationSettings(
+    defaultActionName: 'Open notification',
+  );
+  final initializationSettingsWindows = WindowsInitializationSettings(
+    appName: 'Flutter Local Notifications Example',
+    appUserModelId: 'Com.Dexterous.FlutterLocalNotificationsExample',
+    guid: appGuid,
+  );
+
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsDarwin,
+    macOS: initializationSettingsDarwin,
+    linux: initializationSettingsLinux,
+    windows: initializationSettingsWindows,
+  );
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+    onDidReceiveNotificationResponse: onDidReceiveNotificationResponse,
+  );
+
+  final notificationAppLaunchDetails = !kIsWeb && Platform.isLinux
+      ? null
+      : await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+
+  return notificationAppLaunchDetails;
+}
+
+void onDidReceiveNotificationResponse(
+  NotificationResponse notificationResponse,
+) async {
+  final String? payload = notificationResponse.payload;
+  if (notificationResponse.payload != null) {
+    debugPrint('notification payload: $payload');
+  }
+  // await Navigator.push(
+  //   context,
+  //   MaterialPageRoute<void>(builder: (context) => SecondScreen(payload)),
+  // );
+}
+
+Future<void> scheduleNotification() async {
+  final now = tz.TZDateTime.now(tz.local);
+  final when = tz.TZDateTime(
+    tz.local,
+    now.year,
+    now.month,
+    now.day,
+    now.hour,
+    now.minute,
+  ).add(const Duration(minutes: 1));
+
+  print("Scheduling notification for $when");
+
+  await flutterLocalNotificationsPlugin.zonedSchedule(
+    0, // FIXME: What is this ID?
+    'daily scheduled notification title',
+    'daily scheduled notification body',
+    when,
+    const NotificationDetails(
+      android: AndroidNotificationDetails(
+        channelId,
+        channelName,
+        channelDescription: 'daily notification description',
+      ),
+    ),
+    androidScheduleMode: AndroidScheduleMode.alarmClock,
+    matchDateTimeComponents: DateTimeComponents.time,
+  );
+
+  print("Created zonedSchedule notification");
+}
+
+Future<void> configureLocalTimeZone() async {
+  if (kIsWeb || Platform.isLinux) {
+    return;
+  }
+  tz.initializeTimeZones();
+  if (Platform.isWindows) {
+    return;
+  }
+  final timeZoneName = await FlutterTimezone.getLocalTimezone();
+  tz.setLocalLocation(tz.getLocation(timeZoneName));
+}
+
+AndroidFlutterLocalNotificationsPlugin androidNotificationPlugin() {
+  if (!Platform.isAndroid) {
+    throw Exception('Error: Not running on Android');
+  }
+
+  var plugin = flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin
+      >();
+  if (plugin == null) {
+    throw Exception('Error: Could not get Android notification implementation');
+  }
+
+  return plugin;
+}
+
+Future<bool?> isAndroidPermissionGranted() async {
+  if (Platform.isAndroid) {
+    var canScheduleExact = await androidNotificationPlugin()
+        .canScheduleExactNotifications();
+    var areNotificationsEnabled = await androidNotificationPlugin()
+        .areNotificationsEnabled();
+
+    return canScheduleExact == true && areNotificationsEnabled == true;
+  }
+
+  return null;
+}
+
+Future<bool?> requestPermissions() async {
+  if (Platform.isIOS || Platform.isMacOS) {
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+          IOSFlutterLocalNotificationsPlugin
+        >()
+        ?.requestPermissions(alert: true, badge: true, sound: true);
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+          MacOSFlutterLocalNotificationsPlugin
+        >()
+        ?.requestPermissions(alert: true, badge: true, sound: true);
+  } else if (Platform.isAndroid) {
+    var canScheduleExact = await androidNotificationPlugin()
+        .requestExactAlarmsPermission();
+    var areNotificationsEnabled = await androidNotificationPlugin()
+        .requestNotificationsPermission();
+
+    return canScheduleExact == true && areNotificationsEnabled == true;
+  }
+
+  return null;
+}
+
+/*
+
+For bypassing the DnD setup - requestNotificationPolicyAccess
+
+
+*/
